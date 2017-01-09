0e2e377c1d2ac1e8775a2c5ed76a19de09d9cd35
hv ropert <hugues.vincent.ropert@tetha.pw>
2017-01-09 19:17:23 +0100

    clean main function

--- simplexe.cpp
+++ simplexe.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <regex>
+#include <numeric>
 
 // containers
 #include <vector>
@@ -12,28 +13,38 @@
 #include "parser.h"
 
 using namespace std;
+void lp_full()
+{
+
+}
+void lp_integer()
+{
+	
+}
+void binary(tableau& tab)
+{
+	double z, z_0, z_1;
+	vector<double> thetas, thetas_0, thetas_1;
+	vector<bool> result;
+	
+	for(int i=0; i<tab.get_nb_var(); i++)
+	{
+
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	tableau t, t1;
 	tableau& table=t, rt=t1;
-	// vector<double> v{3, 9};
-	// table.add_row(vector<double> {3, 9}, set<string> {"x", "y"});
-	// table.add_row(vector<double> {1, 19});
-	// table.add_row(vector<double> {1, 19, 22});
-	// table.add_row(vector<double> {2, 1, 99}, set<string> {"z"});
-	// table.add_variable("lol");
-	// table.print();
 	
-	if(argc>1){
-		// ifstream& file = ifstream(argv[1]);
-		if(parser::parse_file(argv[1], rt))
-		{
-			// rt.print();
-		}	
-		else cout << "file isn't readable\n"; 
+	if(argc>1)
+	{
+		if(!parser::parse_file(argv[1], rt)) cout << "file isn't readable\n"; 
 	}
 	else cout << "Can't reach the file\n";
+	
+
 	return 0; 
 }
 
