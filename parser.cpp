2a5ba22079dafa727415ad86ced2565d82e115bd
hv ropert <hugues.vincent.ropert@tetha.pw>
2017-01-11 02:47:00 +0100

    fix parser bug sign after comparator

--- parser.cpp
+++ parser.cpp
@@ -74,7 +74,6 @@ bool parser::fills_vectors_from_line(string& line, tableau& tab)
     		else if(regex_match(last_word, is_sign))
     		{
     			words.push_back(word);
-                cout << line_col_variables[line_col_names.size()] <<" -"<<  word<<"\n";
                 if(regex_match(words[words.size()-3], is_comparator))
                     line_col_variables[line_col_names.size()-1] = line_col_variables.back() * stod(word);
                 else 
